e87bd5fa8525cabab00779c18655cf4adbc63bc5
YashMengji <yashmengji2172005@gmail.com>
2024-12-20 19:38:45 +0530

    TASK 2 COMPLETED

---
+++ TASK2/scripts/index.js
@@ -0,0 +1,86 @@
+const allNavDiv = document.querySelectorAll(".nav");
+allNavDiv.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    if(!element.classList.contains("active")){
+      allNavDiv.forEach((element1) => {
+        if(element1.classList.contains("active")){
+          element1.classList.remove("active");
+        }
+      })
+      element.classList.add("active");
+    }
+  });
+});
+
+// Below for typewrite effect in loop
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.3em solid #fff}";
+  document.body.appendChild(css);
+};
+
+//scroll heading
+const elements = document.querySelectorAll('.fade-in');
+
+  const handleScroll = () => {
+    elements.forEach(element => {
+      const rect = element.getBoundingClientRect();
+      if (rect.top < window.innerHeight && rect.bottom > 0) {
+        element.classList.add('show');
+      }
+    });
+  };
+
+  window.addEventListener('scroll', handleScroll);
+  handleScroll();
